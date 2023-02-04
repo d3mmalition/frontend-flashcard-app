052e60f0927d07092dd6b138a2eec82941167148
d3mmalition <ecarr.gardner@gmail.com>
2023-02-03 21:17:50 -0500

    working components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FlashcardList from './FlashcardList';
 
 function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
   return (
-  <h1>Hello World!</h1>
+  <FlashcardList flashcards={flashcards} />
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id:1,
+    question: "What is React?",
+    answer: "A library for managing user interfaces",
+    options: [
+      'Answer 1',
+      'Answer 2',
+      'Answer 3',
+      'Answer 4',
+      'Answer 5'
+    ]
+  },  
+  {
+    id:2,
+    question: "What is a Single Page Application?",
+    answer: "Write answer here",
+    options: [
+      'Answer 1',
+      'Answer 2',
+      'Answer 3',
+      'Answer 4',
+      'Answer 5'
+    ]
+  }
+]
 
 export default App;
